4d9ca873ffa9b69d2efc4c586f8f2bb739ed8b92
thongwav <thonglv@wav.vn>
2018-10-31 11:57:09 +0700

    31/10

--- gulpfile.js
+++ gulpfile.js
@@ -57,7 +57,7 @@ gulp.task('sass', function () {
 
 gulp.task('include', function () {
     return gulp.src('./layouts/page/*.html')
-        .pipe(includeTag())
+        .pipe(includeTag()).on('error',gulti.log)
         .pipe(htmlbeautify({
             indentSize: 4
         }))
